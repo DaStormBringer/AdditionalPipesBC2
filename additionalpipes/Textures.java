62dbddd0e11865f14b9fe7cdefd74fb354d60e39
tcooc <tco_oc1111@hotmail.com>
2013-04-27 11:23:00 -0400

    Move code.

---
+++ additionalpipes/Textures.java
@@ -0,0 +1,23 @@
+package buildcraft.additionalpipes;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.util.Icon;
+import buildcraft.additionalpipes.textures.APActionTriggerIconProvider;
+import buildcraft.additionalpipes.textures.APPipeIconProvider;
+
+public class Textures {
+	public static APActionTriggerIconProvider actionIconProvider=new APActionTriggerIconProvider();
+	public static APPipeIconProvider pipeIconProvider=new APPipeIconProvider();
+	public static Icon tetherTexture;
+	public static void registerIcons(IconRegister par1IconRegister)
+	{
+		actionIconProvider.registerIcons(par1IconRegister);
+		pipeIconProvider.registerIcons(par1IconRegister);
+		if(par1IconRegister==Minecraft.getMinecraft().renderEngine.textureMapBlocks)
+		{
+			tetherTexture=par1IconRegister.registerIcon("additionalpipes:tether");
+		}
+	}
+}
+
