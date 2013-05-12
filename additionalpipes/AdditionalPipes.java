600d9444e126186c8e1ba7c3308d6665fc8e9970
tcooc <tco_oc1111@hotmail.com>
2013-05-12 15:08:01 -0400

    Build file fixes.

--- additionalpipes/AdditionalPipes.java
+++ additionalpipes/AdditionalPipes.java
@@ -81,7 +81,7 @@ clientSideRequired=true, serverSideRequired=true, packetHandler=NetworkHandler.c
 public class AdditionalPipes {
 	public static final String MODID = "APUnofficial";
 	public static final String NAME = "Additional Pipes Unofficial";
-	public static final String VERSION = "2.2.0";
+	public static final String VERSION = "@AP_VERSION@";
 	public static final String CHANNEL = MODID;
 	public static final String CHANNELNBT = CHANNEL + "NBT";
 
