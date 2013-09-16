a61b41529b95fd5947280fc17fe87627d8fd6007
tcooc <tco_oc1111@hotmail.com>
2013-09-15 21:31:54 -0400

    Fix versioning bug.

--- additionalpipes/AdditionalPipes.java
+++ additionalpipes/AdditionalPipes.java
@@ -74,7 +74,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 public class AdditionalPipes {
 	public static final String MODID = "APUnofficial";
 	public static final String NAME = "Additional Pipes Unofficial";
-	public static final String VERSION = "${env.AP_VERSION}";
+	public static final String VERSION = "@AP_VERSION@";
 	public static final String CHANNEL = MODID;
 	public static final String CHANNELNBT = CHANNEL + "NBT";
 
