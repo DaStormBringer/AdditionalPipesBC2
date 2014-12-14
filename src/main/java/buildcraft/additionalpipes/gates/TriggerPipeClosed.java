52b733c0e47b40212407bc7de57b46a2a38cbe91
multiplemonomials <jsmith@crackofdawn.onmicrosoft.com>
2014-12-14 09:23:33 -0800

    Fix missing ".name"'s in language files

--- src/main/java/buildcraft/additionalpipes/gates/TriggerPipeClosed.java
+++ src/main/java/buildcraft/additionalpipes/gates/TriggerPipeClosed.java
@@ -1,14 +1,9 @@
 package buildcraft.additionalpipes.gates;
 
-import java.util.Collection;
-
-import net.minecraft.tileentity.TileEntity;
-import net.minecraftforge.common.util.ForgeDirection;
 import buildcraft.additionalpipes.pipes.PipeItemsClosed;
 import buildcraft.api.statements.IStatement;
 import buildcraft.api.statements.IStatementContainer;
 import buildcraft.api.statements.IStatementParameter;
-import buildcraft.api.statements.ITriggerExternal;
 import buildcraft.api.statements.ITriggerInternal;
 import buildcraft.transport.TileGenericPipe;
 
