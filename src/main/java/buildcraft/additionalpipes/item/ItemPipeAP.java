03330e10cc8f611e6ea785ee60bfde99b8eb59aa
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2015-02-18 10:30:47 -0800

    add ItemPipeAP

---
+++ src/main/java/buildcraft/additionalpipes/item/ItemPipeAP.java
@@ -0,0 +1,36 @@
+package buildcraft.additionalpipes.item;
+
+import java.util.List;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.EnumRarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.StatCollector;
+import buildcraft.core.CreativeTabBuildCraft;
+import buildcraft.transport.ItemPipe;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+//special pipe code
+public class ItemPipeAP extends ItemPipe
+{
+	public ItemPipeAP() {
+		super(CreativeTabBuildCraft.PIPES);
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public EnumRarity getRarity(ItemStack stack) {
+		return EnumRarity.rare;
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean advanced) {
+		super.addInformation(stack, player, list, advanced);
+		String key = "tip." + stack.getItem().getClass().getSimpleName();
+		
+		list.add(StatCollector.translateToLocal(key));
+	}
+}
