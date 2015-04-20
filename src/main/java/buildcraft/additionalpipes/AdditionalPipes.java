554469a4e25ab9b08fa51b1b3cb1587281c0b90e
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2015-04-19 20:01:14 -0700

    Update version number

--- src/main/java/buildcraft/additionalpipes/AdditionalPipes.java
+++ src/main/java/buildcraft/additionalpipes/AdditionalPipes.java
@@ -63,7 +63,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 public class AdditionalPipes {
 	public static final String MODID = "additionalpipes";
 	public static final String NAME = "Additional Pipes";
-	public static final String VERSION = "4.3.0";
+	public static final String VERSION = "4.3.1";
 
 	@Instance(MODID)
 	public static AdditionalPipes instance;
