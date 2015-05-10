9744ac44d53335e06ae435019a5b88a408e05590
multiplemonomials <jsmith@crackofdawn.onmicrosoft.com>
2015-05-09 22:05:33 -0700

    Update version number.
    Also, I couldn't resist making another texture for the new debugger
    item.

--- src/main/java/buildcraft/additionalpipes/AdditionalPipes.java
+++ src/main/java/buildcraft/additionalpipes/AdditionalPipes.java
@@ -66,7 +66,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 public class AdditionalPipes {
 	public static final String MODID = "additionalpipes";
 	public static final String NAME = "Additional Pipes";
-	public static final String VERSION = "4.4.0";
+	public static final String VERSION = "4.5.0";
 
 	@Instance(MODID)
 	public static AdditionalPipes instance;
