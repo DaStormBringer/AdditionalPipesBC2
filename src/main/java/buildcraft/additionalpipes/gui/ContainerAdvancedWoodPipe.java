042be01f12bc80ff8df81b8f4c1487f9df1f35da
TheAndrey <andrey9506@yandex.ru>
2017-05-31 14:03:21 +0200

    Fixed closing containers (inventory duplication)
    Containers must be closed when block broken or player too far away.

--- src/main/java/buildcraft/additionalpipes/gui/ContainerAdvancedWoodPipe.java
+++ src/main/java/buildcraft/additionalpipes/gui/ContainerAdvancedWoodPipe.java
@@ -14,6 +14,7 @@ import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.Slot;
 import buildcraft.additionalpipes.pipes.PipeTransportAdvancedWood;
 import buildcraft.core.lib.gui.BuildCraftContainer;
+import buildcraft.transport.TileGenericPipe;
 
 public class ContainerAdvancedWoodPipe extends BuildCraftContainer {
 
@@ -44,6 +45,9 @@ public class ContainerAdvancedWoodPipe extends BuildCraftContainer {
 
 	@Override
 	public boolean canInteractWith(EntityPlayer entityplayer) {
+		TileGenericPipe tile = logic.container;
+		if(tile.getWorldObj().getTileEntity(tile.xCoord, tile.yCoord, tile.zCoord) != tile) return false;
+		if(entityplayer.getDistanceSq(tile.xCoord + 0.5D, tile.yCoord + 0.5D, tile.zCoord + 0.5D) > 64) return false;
 		return true;
 	}
 
